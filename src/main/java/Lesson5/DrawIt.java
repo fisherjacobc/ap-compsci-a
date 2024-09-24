d57b67c01086bf9088f23918ad84cce9e4afb5a0
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-09-24 11:47:09 +0000

    upload work from 9/24

--- src/main/java/Lesson5/DrawIt.java
+++ src/main/java/Lesson5/DrawIt.java
@@ -1,34 +1,33 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
 
-import javax.swing.JFrame; 
+import javax.swing.JFrame;
 import java.awt.Component;
-import java.awt.Graphics;
 import java.awt.Color;
 
-public class DrawIt extends JFrame
-{
+/**
+ * @author Jacob Fisher
+ * @created 24 Sep 2024
+ */
+public class DrawIt extends JFrame {
 	public static int WIDTH = 800;
 	public static int HEIGHT = 600;
 
-	public DrawIt()
-	{
+	public DrawIt() {
 		super("DrawIt");
 
 		setSize(WIDTH, HEIGHT);
 		setBackground(Color.BLACK);
 
 		Tablet tb = new Tablet(this);
-		((Component)tb).setFocusable(true);
+		((Component) tb).setFocusable(true);
 
-      getContentPane().add( tb );
+		getContentPane().add(tb);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 
-	public static void main( String args[] )
-	{
-		DrawIt run = new DrawIt();
+	public static void main(String args[]) {
+		new DrawIt();
 	}
 }
