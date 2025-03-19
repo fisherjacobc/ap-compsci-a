3f04df4c95538c12aa5389630123f0b44d2b22a5
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-03-19 11:42:57 +0000

    update oldmacdonald

--- src/main/java/EIEIO/OldMacDonald.java
+++ src/main/java/EIEIO/OldMacDonald.java
@@ -1,10 +1,10 @@
 public class OldMacDonald {
     public static void main(String[] args) {
-        // Farm farm = new Farm();
+        Farm farm = new Farm();
 
-        // farm.animalSounds();
+        farm.animalSounds();
 
-        Chick chick = new Chick(true);
-        System.out.println(chick.getType() + " goes " + chick.getSound());
+        // Chick chick = new Chick(true);
+        // System.out.println(chick.getType() + " goes " + chick.getSound());
     }
 }
